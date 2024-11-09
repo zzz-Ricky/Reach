3fc2932f39d55e22df0d9a4b45c91d123b808185
Abdirazak Yusuf <abdirazak140@gmail.com>
2024-11-09 18:54:26 -0500

    Added ask

--- my-app/app/(tabs)/_layout.tsx
+++ my-app/app/(tabs)/_layout.tsx
@@ -22,7 +22,7 @@ export default function TabLayout() {
             <Tabs.Screen
                 name="request"
                 options={{
-                    title: 'Request',
+                    title: 'Requests',
                     tabBarIcon: ({ color }) => <FontAwesome size={28} name="group" color={color} />,
                 }}
             />
